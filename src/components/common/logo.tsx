8f3b1e032c9f7e27ed87ad9f5574aca6e6a3d25c
abs0luty <adisalimgereev@gmail.com>
2025-04-11 23:36:24 +0500

    Will memo solve the problem?

--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -1,7 +1,8 @@
+import React from "react"
 import { Link } from "react-router-dom"
 
-export const Logo: React.FC = () => (
+export const Logo: React.FC = React.memo(() => (
   <Link to="/">
     <img className="p-2" src="logo.svg" alt="logo" />
   </Link>
-)
+))
