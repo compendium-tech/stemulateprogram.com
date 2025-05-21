5f5cfcf8ba8efe92a4bc36cc952b0d5630f41b4e
abs0luty <adisalimgereev@gmail.com>
2025-05-22 00:28:47 +0500

    Yeah

--- src/pages/publications-page.tsx
+++ src/pages/publications-page.tsx
@@ -1,64 +1,63 @@
-import { Button } from "@/components/ui/button"
 import { StandartLayout } from "@/layout/navbar"
 import { Link } from "react-router-dom"
 
-const publications = [
-  {
-    name: `Explainable AI in Healthcare: Interpreting Machine Learning’s
-Dementia Classification On Imbalanced Multi-Domain Clinical Data`,
-    student: "Tuan Anh Ngo",
-    pdfLink: "/papers/Paper_TuanAnhNgo.pdf",
-    presentationLink: "/presentations/Presentation_TuanAnhNgo.pptx",
-  },
-  {
-    name: `Hosting the Olympics: a comparative study of developed and
-developing economies.`,
-    student: "Yesseniya Zhumagatova",
-    pdfLink: "/papers/Paper_Yesseniya_Zhumagatova.pdf",
-    presentationLink: "/presentations/Presentation_Yesseniya_Zhumagatova.pdf",
-  },
-  {
-    name: "Relevance and impact of neurography in Kazakhstan",
-    student: "Alsana",
-    pdfLink: "/papers/Paper_Alsana.pdf",
-    presentationLink: "/presentations/Presentation_Alsana.pptx",
-  },
-  {
-    name: `Do the changes in human microbiome affect the development of autoimmune
-diseases and can the study of these changes fundamentally alter treatment protocols
-and approaches to autoimmune diseases?`,
-    student: "Amina Smak",
-    pdfLink: "/papers/Paper_Amina_Smak.pdf",
-    presentationLink: "/presentations/Presentation_Amina_Smak.pdf",
-  },
-  {
-    name: `Why does Kazakhstan have a more developed startup ecosystem
-than Kyrgyzstan, although both share a similar Soviet past?`,
-    student: "Amir Bushumbayev",
-    pdfLink: "/papers/Paper_Amir_Bushumbayev.pdf",
-    presentationLink: "/presentations/Presentation_Amir_Bushumbayev.pdf",
-  },
-  {
-    name: `Why is Kazakhstani education system in public schools not as
-successful in promoting creativity compared to specialized/private schools?`,
-    student: "Aruzhan Olzhabay",
-    pdfLink: "/papers/Paper_Aruzhan_Olzhabay.pdf",
-    presentationLink: "/presentations/Presentation_Aruzhan_Olzhabay.pdf",
-  },
-  {
-    name: `How do migraines affect the brain and our cognitive skills?`,
-    student: "Bolat Leila",
-    pdfLink: "/papers/Paper_Bolat_Leila.pdf",
-    presentationLink: "/presentations/Presentation_Bolat_Leila.pdf",
-  },
-  {
-    name: `How can dynamic pricing models be successfully implemented into
-digital marketplaces to benefit SMEs in Kazakhstan?`,
-    student: "Safiyakhon Zulfikarova",
-    pdfLink: "/papers/Paper_Safiyakhon_Zulfikarova.pdf",
-    presentationLink: "/pdf/presentation2.pdf",
-  },
-]
+// const publications = [
+//   {
+//     name: `Explainable AI in Healthcare: Interpreting Machine Learning’s
+// Dementia Classification On Imbalanced Multi-Domain Clinical Data`,
+//     student: "Tuan Anh Ngo",
+//     pdfLink: "/papers/Paper_TuanAnhNgo.pdf",
+//     presentationLink: "/presentations/Presentation_TuanAnhNgo.pptx",
+//   },
+//   {
+//     name: `Hosting the Olympics: a comparative study of developed and
+// developing economies.`,
+//     student: "Yesseniya Zhumagatova",
+//     pdfLink: "/papers/Paper_Yesseniya_Zhumagatova.pdf",
+//     presentationLink: "/presentations/Presentation_Yesseniya_Zhumagatova.pdf",
+//   },
+//   {
+//     name: "Relevance and impact of neurography in Kazakhstan",
+//     student: "Alsana",
+//     pdfLink: "/papers/Paper_Alsana.pdf",
+//     presentationLink: "/presentations/Presentation_Alsana.pptx",
+//   },
+//   {
+//     name: `Do the changes in human microbiome affect the development of autoimmune
+// diseases and can the study of these changes fundamentally alter treatment protocols
+// and approaches to autoimmune diseases?`,
+//     student: "Amina Smak",
+//     pdfLink: "/papers/Paper_Amina_Smak.pdf",
+//     presentationLink: "/presentations/Presentation_Amina_Smak.pdf",
+//   },
+//   {
+//     name: `Why does Kazakhstan have a more developed startup ecosystem
+// than Kyrgyzstan, although both share a similar Soviet past?`,
+//     student: "Amir Bushumbayev",
+//     pdfLink: "/papers/Paper_Amir_Bushumbayev.pdf",
+//     presentationLink: "/presentations/Presentation_Amir_Bushumbayev.pdf",
+//   },
+//   {
+//     name: `Why is Kazakhstani education system in public schools not as
+// successful in promoting creativity compared to specialized/private schools?`,
+//     student: "Aruzhan Olzhabay",
+//     pdfLink: "/papers/Paper_Aruzhan_Olzhabay.pdf",
+//     presentationLink: "/presentations/Presentation_Aruzhan_Olzhabay.pdf",
+//   },
+//   {
+//     name: `How do migraines affect the brain and our cognitive skills?`,
+//     student: "Bolat Leila",
+//     pdfLink: "/papers/Paper_Bolat_Leila.pdf",
+//     presentationLink: "/presentations/Presentation_Bolat_Leila.pdf",
+//   },
+//   {
+//     name: `How can dynamic pricing models be successfully implemented into
+// digital marketplaces to benefit SMEs in Kazakhstan?`,
+//     student: "Safiyakhon Zulfikarova",
+//     pdfLink: "/papers/Paper_Safiyakhon_Zulfikarova.pdf",
+//     presentationLink: "/pdf/presentation2.pdf",
+//   },
+// ]
 
 const PublicationsPageContent = () => {
   return (
