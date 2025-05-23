e27edec08859c597939ab4a04cfb22d0e835e958
abs0luty <adisalimgereev@gmail.com>
2025-05-23 12:32:54 +0500

    Shit

--- src/components/landing/countdown.tsx
+++ src/components/landing/countdown.tsx
@@ -80,7 +80,6 @@ export function Countdown(props: {
                       {props.targetDate.toLocaleDateString()}
                     </span>
                     . {props.deadlineDescriptionText}
-                    {props.deadlineDescriptionText}
                   </p>
                 </div>
               </div>
@@ -102,7 +101,7 @@ export function Countdown(props: {
 function TimeUnit({ value, label }: { value: number; label: string }) {
   return (
     <div className="flex flex-col items-center">
-      <div className="bg-neutral-900 px-4 py-3 rounded-2xl border border-gray-200 flex items-center justify-center">
+      <div className="bg-neutral-900 px-3 py-2 rounded-2xl border border-gray-200 flex items-center justify-center">
         <span className="text-2xl sm:text-3xl font-bold">
           {value < 10 ? `0${value}` : value}
         </span>
