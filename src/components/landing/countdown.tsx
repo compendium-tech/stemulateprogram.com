47f83b715cef6d43f2bd3e03a7a2971eb98a416d
abs0luty <adisalimgereev@gmail.com>
2025-06-06 04:23:37 +0500

    Make text more stable

--- src/components/landing/countdown.tsx
+++ src/components/landing/countdown.tsx
@@ -102,7 +102,7 @@ function TimeUnit({ value, label }: { value: number; label: string }) {
   return (
     <div className="flex flex-col items-center">
       <div className="bg-neutral-900 px-3 py-2 rounded-2xl border border-gray-200 border-dashed flex items-center justify-center">
-        <span className="text-2xl sm:text-3xl font-bold">
+        <span className="text-2xl sm:text-3xl font-bold text-center w-[1.3em]">
           {value < 10 ? `0${value}` : value}
         </span>
       </div>
