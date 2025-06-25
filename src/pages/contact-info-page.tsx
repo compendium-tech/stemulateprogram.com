aea6534edbb404620022ea3388d807a8130dbe58
abs0luty <adisalimgereev@gmail.com>
2025-06-26 02:31:12 +0500

    Fix contact info page

--- src/pages/contact-info-page.tsx
+++ src/pages/contact-info-page.tsx
@@ -8,14 +8,12 @@ const ContactInfoPageContent: FC = () => {
         <p className="font-bold text-3xl sm:text-4xl md:text-5xl font-serif">
           Contact Information
         </p>
-        <p className="text-lg md:text-xl">
-          <b>
-            We're here to help! Please feel free to reach out to us with any
-            inquiries.
-          </b>
+        <p className="text-base md:text-xl">
+          We're here to help! Please feel free to reach out to us with any
+          inquiries.
         </p>
-        <p className="text-lg md:text-xl">
-          Email address:{" "}
+        <p className="text-base md:text-xl">
+          Email:{" "}
           <a
             href="mailto:admissions@stemulateprogram.com"
             className="hover:underline text-red-600 hover:text-red-800"
@@ -31,12 +29,9 @@ const ContactInfoPageContent: FC = () => {
             +7 (775) 303-6077
           </a>
         </p>
-        <p className="text-lg md:text-xl">
+        <p className="text-base md:text-xl">
           We aim to respond to all inquiries within 24-48 business hours. <br />
-          Our operating hours are: Monday - Friday, 9:00 AM - 5:00 PM (Eastern
-          Time).
         </p>
-        <img src="logo2.svg" className="w-48" />
       </div>
     </main>
   )
